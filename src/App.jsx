ca9dbe26476442f2cdf509d6838496943cea5b4f
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-01-10 12:50:38 +0530

    files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="/images/shoe-logo.png" alt="Nike Logo" srcset="" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  )
+}
+
+export default App
